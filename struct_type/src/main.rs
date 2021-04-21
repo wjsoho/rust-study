9e6e76d3bd184792af7a5691ca6d18afb090c3a9
肖人祥 <renxiang.xiao@zqgame.com>
2021-04-21 21:55:25 +0800

    commit

--- struct_type/src/main.rs
+++ struct_type/src/main.rs
@@ -1,4 +1,32 @@
 fn main() {
-    println!("Hello, world!");
+    demo1()
 }
 
+#[derive(Debug)]
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+// 定义一个结构体
+fn demo1() {
+    let user = User {
+        username: String::from("A"),
+        email: String::from("a@163.com"),
+        sign_in_count:1,
+        active:false,
+    };
+    print!("user:{:?}",user)
+}
+
+
+/*
+struct User {
+username: String,
+email: String,
+sign_in_count: u64,
+active: bool,
+}
+*/
